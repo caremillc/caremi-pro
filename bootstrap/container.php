6f9a23b9785d5c9eb0fe696f43a8c649fd530bf2
caremi <Willy Nilly>
2025-05-14 23:32:36 +0400

    Container Parameters

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,14 +1,22 @@
-<?php declare(strict_types=1); // config/container.php
+<?php // config/container.php
 
 $container = new \League\Container\Container();
 
+#parameters
+// Load application routes from an external configuration file.
+$routes = include BASE_PATH . '/routes/web.php';
 
 # services
 # add alias for Router class, 
 $container->add(\Careminate\Routing\RouterInterface::class, \Careminate\Routing\Router::class);
 
+// Extend RouterInterface definition to inject routes
+$container->extend(Careminate\Routing\RouterInterface::class)
+          ->addMethodCall('setRoutes',[new League\Container\Argument\Literal\ArrayArgument($routes)]);
+          
 $container->add(Careminate\Http\Kernel::class)
           ->addArgument(Careminate\Routing\RouterInterface::class);
 
-dd($container);
+// dd($container);
+
 return $container;
