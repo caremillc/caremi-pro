395bfc5877a9ecf6100020f262327e99848a1ade
caremi <Willy Nilly>
2025-05-14 22:49:52 +0400

    Container Aliases for router

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,6 +1,14 @@
-<?php // config/container.php
+<?php declare(strict_types=1); // config/container.php
 
 $container = new \League\Container\Container();
+
+
+# services
+# add alias for Router class, 
+$container->add(\Careminate\Routing\RouterInterface::class, \Careminate\Routing\Router::class);
+
+$container->add(Careminate\Http\Kernel::class)
+          ->addArgument(Careminate\Routing\RouterInterface::class);
 
 dd($container);
 return $container;
