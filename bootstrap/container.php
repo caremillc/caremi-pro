bedf20aca70b186a79f539b1f1fbeaaa86c6c6eb
caremi <Willy Nilly>
2025-05-15 00:39:28 +0400

    add environment variables

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,26 +1,38 @@
 <?php declare(strict_types=1); // config/container.php
+// Load environment variables
+$dotenv = new \Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(dirname(__DIR__) . '/.env');
 
 $container = new \League\Container\Container();
 
 // Enable auto-resolution of dependencies through reflection
 $container->delegate(new \League\Container\ReflectionContainer(true));
 
+#env parameters
+$appEnv     = env('APP_ENV', 'production'); // Default to 'production' if not set
+$appKey     = env('APP_KEY');               // Default to 'production' if not set
+$appVersion = env('APP_VERSION');
+
+$container->add('APP_ENV', new \League\Container\Argument\Literal\StringArgument($appEnv));
+$container->add('APP_KEY', new \League\Container\Argument\Literal\StringArgument($appKey));
+$container->add('APP_VERSION', new \League\Container\Argument\Literal\StringArgument($appVersion));
+
 #parameters
 // Load application routes from an external configuration file.
 $routes = include BASE_PATH . '/routes/web.php';
 
 # services
-# add alias for Router class, 
+# add alias for Router class,
 $container->add(\Careminate\Routing\RouterInterface::class, \Careminate\Routing\Router::class);
 
 // Extend RouterInterface definition to inject routes
 $container->extend(Careminate\Routing\RouterInterface::class)
-          ->addMethodCall('setRoutes',[new League\Container\Argument\Literal\ArrayArgument($routes)]);
-          
- $container->add(Careminate\Http\Kernel::class)
-          ->addArgument(Careminate\Routing\RouterInterface::class)
-          ->addArgument($container);
+    ->addMethodCall('setRoutes', [new League\Container\Argument\Literal\ArrayArgument($routes)]);
 
-//  dd($container);
+$container->add(Careminate\Http\Kernel::class)
+    ->addArgument(Careminate\Routing\RouterInterface::class)
+    ->addArgument($container);
+
+dd($container);
 
 return $container;
