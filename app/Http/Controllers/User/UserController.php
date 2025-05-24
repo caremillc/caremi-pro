b617c9dbc90f1a9c765448a877f99cb80fc1036c
caremi <Willy Nilly>
2025-05-24 17:24:46 +0400

    Submitting a user Form

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Careminate\Http\Responses\Response;
+
+class UserController extends Controller
+{
+    public function index(): Response
+    { 
+        $users = "All users";
+
+       return view('users/index.html.twig', compact('users'));
+    }
+
+    public function create(): Response
+    {
+        // Your logic here
+        return view('users/create.html.twig');
+    }
+
+    public function store(): Response
+    {
+        // Your logic here
+        return new Response('<h1>Store Post</h1>');
+    }
+
+    public function show(int $id): Response
+    {
+        // Your logic here
+        $postId = "<h1>Show Post with ID: $id</h1>";
+        return view('users/show.html.twig', compact('postId'));
+    }
+
+    public function edit(int $id): Response
+    {
+        // Your logic here
+        $postId = "<h1>Edit Post with ID: $id</h1>";
+        return view('users/edit.html.twig', compact('postId'));
+    }
+
+    public function update(int $id): Response
+    {
+        // Your logic here
+        return new Response("<h1>Update Post with ID: $id</h1>");
+    }
+
+    public function delete(int $id): Response
+    {
+        // Your logic here
+        return new Response("<h1>Delete Post with ID: $id</h1>");
+    }
+}
+ 
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,16 @@ return [
    ['PUT', '/posts/{id}/update', [App\Http\Controllers\Post\PostController::class, 'update']],
    ['DELETE', '/posts/{id}/delete', [App\Http\Controllers\Post\PostController::class, 'delete']],
 
+   //users
+   ['GET', '/users', [App\Http\Controllers\User\UserController::class, 'index']],
+   ['GET', '/users/create', [App\Http\Controllers\User\UserController::class, 'create']],
+   ['POST', '/users/store', [App\Http\Controllers\User\UserController::class, 'store']],
+   ['GET', '/users/{id}/show', [\App\Http\Controllers\User\UserController::class, 'show']],
+   ['GET', '/users/{id}/edit', [App\Http\Controllers\User\UserController::class, 'edit']],
+   ['PUT', '/users/{id}/update', [App\Http\Controllers\User\UserController::class, 'update']],
+   ['DELETE', '/users/{id}/delete', [App\Http\Controllers\User\UserController::class, 'delete']],
+
+
 //response
    ['GET', '/Hello/{name:.+}', function (string $name) {
        return new Response("Hello $name");
