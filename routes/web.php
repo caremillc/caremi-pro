a3340f20329d44c7cd03c6deefb11feaf92cdbe4
caremi <Willy Nilly>
2025-05-24 16:48:30 +0400

    Reusable Templates

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,23 @@
 
 use Careminate\Http\Responses\Response;
 
-return [
-    ['GET', '/', [App\Http\Controllers\HomeController::class, 'index']],
 
-    ['GET', '/posts', [App\Http\Controllers\Post\PostController::class, 'index']],
-    ['GET', '/posts/create', [App\Http\Controllers\Post\PostController::class, 'create']],
-    ['POST', '/posts/store', [App\Http\Controllers\Post\PostController::class, 'store']],
-    ['GET', '/posts/{id:\d+}/show', [\App\Http\Controllers\Post\PostController::class, 'show']],
-    ['GET', '/posts/{id:\d+}/edit', [App\Http\Controllers\Post\PostController::class, 'edit']],
-    ['PUT', '/posts/{id:\d+}/update', [App\Http\Controllers\Post\PostController::class, 'update']],
-    ['DELETE', '/posts/{id:\d+}/delete', [App\Http\Controllers\Post\PostController::class, 'delete']],
+return [
+   //home
+   ['GET', '/', [App\Http\Controllers\HomeController::class, 'index']],
+
+//posts
+   ['GET', '/posts', [App\Http\Controllers\Post\PostController::class, 'index']],
+   ['GET', '/posts/create', [App\Http\Controllers\Post\PostController::class, 'create']],
+   ['POST', '/posts/store', [App\Http\Controllers\Post\PostController::class, 'store']],
+   ['GET', '/posts/{id}/show', [\App\Http\Controllers\Post\PostController::class, 'show']],
+   ['GET', '/posts/{id}/edit', [App\Http\Controllers\Post\PostController::class, 'edit']],
+   ['PUT', '/posts/{id}/update', [App\Http\Controllers\Post\PostController::class, 'update']],
+   ['DELETE', '/posts/{id}/delete', [App\Http\Controllers\Post\PostController::class, 'delete']],
+
 //response
-    ['GET', '/Hello/{name:.+}', function (string $name) {
-        return new Response("Hello $name");
-    }],
+   ['GET', '/Hello/{name:.+}', function (string $name) {
+       return new Response("Hello $name");
+   }],
+
 ];
